22fbc875e382c8ce93d02375259faccc1c37dbef
Chtholly344 <3528497818@qq.con>
2023-03-30 10:39:02 +0800

    修复构建报错

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="../docs/intro">
+            to="../docs/joinzrx">
             -点此查看入团指南-
           </Link>
         </div>
